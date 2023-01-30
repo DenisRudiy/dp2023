df77e123cccaf13f6f4639306a4f4be378a96e3d
DenisRudiy <denryd2311@gmail.com>
2023-01-30 14:53:16 +0200

    add product and info pages + make routes

--- lab1/front-end/src/app/app-routing-module.ts
+++ lab1/front-end/src/app/app-routing-module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { FeaturedProductComponent } from './featured-product/featured-product.component';
-import { CompaniesComponent } from './companies/companies.component';
-import { FooterComponent } from './footer/footer.component';
+
 import { HomePageComponent } from './home-page/home-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { InfoPageComponent } from './info-page/info-page.component';
 
 const appRoute: Routes = [
     {path: '', component: HomePageComponent},
+    {path: 'products', component: ProductsPageComponent},
+    {path: 'info', component: InfoPageComponent},
   ]
 
 @NgModule({
--- lab1/front-end/src/app/app.module.ts
+++ lab1/front-end/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { FeaturedProductComponent } from './featured-product/featured-product.co
 import { CompaniesComponent } from './companies/companies.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { InfoPageComponent } from './info-page/info-page.component';
+import { ProductTitleComponent } from './product-title/product-title.component';
+import { ProductContentComponent } from './product-content/product-content.component';
+import { InfoTitleComponent } from './info-title/info-title.component';
+import { InfoContentComponent } from './info-content/info-content.component';
 
 
 @NgModule({
@@ -20,7 +26,13 @@ import { HomePageComponent } from './home-page/home-page.component';
     FeaturedProductComponent,
     CompaniesComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    ProductsPageComponent,
+    InfoPageComponent,
+    ProductTitleComponent,
+    ProductContentComponent,
+    InfoTitleComponent,
+    InfoContentComponent
   ],
   imports: [
     BrowserModule,

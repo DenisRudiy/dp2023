194b1eea5abddac7c6d9dedd02c2dfecf4f87a7c
DenisRudiy <denryd2311@gmail.com>
2023-02-09 13:47:33 +0200

    add add-form-component

--- lab2/front-end/src/app/app.module.ts
+++ lab2/front-end/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CompaniesComponent } from './companies/companies.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { EditFormComponent } from './edit-form/edit-form.component';
+import { AddFormComponent } from './add-form/add-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { EditFormComponent } from './edit-form/edit-form.component';
     CompaniesComponent,
     FooterComponent,
     HomePageComponent,
-    EditFormComponent
+    EditFormComponent,
+    AddFormComponent
   ],
   imports: [
     BrowserModule,
